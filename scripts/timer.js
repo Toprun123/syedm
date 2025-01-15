584d99d7b2e620733fe50b8f9e3170904ec75831
Syed Daanish <me@syedm.dev>
2025-01-16 01:06:41 +0300

    Add support for editing vaules in newtab page.

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,63 +1,84 @@
-const today = new Date().toLocaleDateString('en-GB').split('/').reverse().join('-');
+const today = new Date()
+  .toLocaleDateString("en-GB")
+  .split("/")
+  .reverse()
+  .join("-");
 var pub_data;
 var timer = new Timer();
 
-setTimeout(() => {
+setTimeout(
+  () => {
     reload();
-}, new Date().setHours(24, 0, 0, 0) - Date.now());
+  },
+  new Date().setHours(24, 0, 0, 0) - Date.now(),
+);
 
-id("start-btn").onclick   = _e => timer.running ? timer.pause() : timer.start();
-id("restart-btn").onclick = _e => timer.reset();
+id("start-btn").onclick = (_e) =>
+  timer.running ? timer.pause() : timer.start();
+id("restart-btn").onclick = (_e) => timer.reset();
 
 reload();
 update();
 
 function reload() {
-    fetch(  "https://api.aladhan.com/v1/timingsByCity/"
-            +today
-            +"?city=Jeddah&country=Saudi+Arabia&method=4&adjustment=1")
-       .then(response => response.json())
-       .then(data => {
-           pub_data = data.data;
-           elem("date-text"    , pub_data.date.hijri.weekday.en
-                               + ", " + pub_data.date.hijri.day
-                               + " " + pub_data.date.hijri.month.en);
-           elem("date"         , pub_data.date.hijri.date);
-           elem("fajr-time"    , ampm(pub_data.timings.Fajr));
-           elem("dhuhr-time"   , ampm(pub_data.timings.Dhuhr));
-           elem("asr-time"     , ampm(pub_data.timings.Asr));
-           elem("maghrib-time" , ampm(pub_data.timings.Maghrib));
-           elem("isha-time"    , ampm(pub_data.timings.Isha));
-       })
-       .catch(error => console.error(error));
+  fetch(
+    "https://api.aladhan.com/v1/timingsByCity/" +
+      today +
+      "?city=Jeddah&country=Saudi+Arabia&method=4&adjustment=1",
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      pub_data = data.data;
+      elem(
+        "date-text",
+        pub_data.date.hijri.weekday.en +
+          ", " +
+          pub_data.date.hijri.day +
+          " " +
+          pub_data.date.hijri.month.en,
+      );
+      elem("date", pub_data.date.hijri.date);
+      elem("fajr-time", ampm(pub_data.timings.Fajr));
+      elem("dhuhr-time", ampm(pub_data.timings.Dhuhr));
+      elem("asr-time", ampm(pub_data.timings.Asr));
+      elem("maghrib-time", ampm(pub_data.timings.Maghrib));
+      elem("isha-time", ampm(pub_data.timings.Isha));
+    })
+    .catch((error) => console.error(error));
 }
 
 function update() {
-    if (pub_data) {
-        elem("total-time" , "Time spent today: "            + timer.getPretty() );
-        elem("run-time"   , "Time spent since last break: " + timer.getPretty(1));
-        var times_rel_prayer = [
-            con(pub_data.timings.Fajr)    - Date.now(),
-            con(pub_data.timings.Dhuhr)   - Date.now(),
-            con(pub_data.timings.Asr)     - Date.now(),
-            con(pub_data.timings.Maghrib) - Date.now(),
-            con(pub_data.timings.Isha)    - Date.now()
-        ];
-        var time_to_next_prayer = prettify(Math.min(...times_rel_prayer.filter(x=>x>0))||0);
-        var time_past_prayer    = (Math.abs(Math.max(...times_rel_prayer.filter(x=>x<0)))||0)/60000;
-        if (time_to_next_prayer != "Infinity:NaN:NaN.N") {
-            elem("prayer-time", "Time to next prayer: "+time_to_next_prayer);
-        } else {
-            elem("prayer-time", "No prayers left for today!");
-        }
-        if (timer.running && time_past_prayer > 7 && time_past_prayer < 8) {
-            timer.pause();
-            play();
-        }
-        if (timer.running && timer.get(1)>1200) {
-            timer.pause();
-            play();
-        }
+  if (pub_data) {
+    elem("total-time", "Time spent today: " + timer.getPretty());
+    elem("run-time", "Time spent since last break: " + timer.getPretty(1));
+    var times_rel_prayer = [
+      con(pub_data.timings.Fajr) - Date.now(),
+      con(pub_data.timings.Dhuhr) - Date.now(),
+      con(pub_data.timings.Asr) - Date.now(),
+      con(pub_data.timings.Maghrib) - Date.now(),
+      con(pub_data.timings.Isha) - Date.now(),
+    ];
+    var time_to_next_prayer = prettify(
+      Math.min(...times_rel_prayer.filter((x) => x > 0)) || 0,
+    );
+    var time_past_prayer =
+      (Math.abs(Math.max(...times_rel_prayer.filter((x) => x < 0))) || 0) /
+      60000;
+    if (time_to_next_prayer != "Infinity:NaN:NaN.N") {
+      elem("prayer-time", "Time to next prayer: " + time_to_next_prayer);
+    } else {
+      elem("prayer-time", "No prayers left for today!");
     }
-    requestIdleCallback(update);
+    if (timer.running && time_past_prayer > 7 && time_past_prayer < 8) {
+      timer.pause();
+      play();
+    }
+    if (timer.running && timer.get(1) > 1200) {
+      timer.pause();
+      play();
+    }
+  }
+  requestIdleCallback(update);
 }
+
+console.log(toolbar);
--- scripts/timer_declarations.js
+++ scripts/timer_declarations.js
@@ -1,59 +1,65 @@
 class Timer {
-    constructor() {
-        this.startTime = 0;
-        this.elapsedTime = 0;
-        this.playTime = 0;
-        this.startPlayTime = 0;
-        this.running = 0;
+  constructor() {
+    this.startTime = 0;
+    this.elapsedTime = 0;
+    this.playTime = 0;
+    this.startPlayTime = 0;
+    this.running = 0;
+  }
+  start() {
+    if (!this.running) {
+      this.startTime = Date.now() - this.elapsedTime;
+      this.startPlayTime = Date.now();
+      this.running = 1;
+      this._update();
     }
-    start() {
-        if (!this.running) {
-            this.startTime = Date.now() - this.elapsedTime;
-            this.startPlayTime = Date.now();
-            this.running = 1;
-            this._update();
-        }
+  }
+  pause() {
+    this.running ? (this.running = this.playTime = 0) : 0;
+  }
+  reset() {
+    this.pause();
+    this.elapsedTime = this.playTime = 0;
+  }
+  _update() {
+    if (this.running) {
+      this.elapsedTime = Date.now() - this.startTime;
+      this.playTime = Date.now() - this.startPlayTime;
+      requestIdleCallback(() => this._update());
     }
-    pause() {
-        this.running ? this.running = this.playTime = 0 : 0;
-    }
-    reset() {
-        this.pause();
-        this.elapsedTime = this.playTime = 0;
-    }
-    _update() {
-        if (this.running) {
-            this.elapsedTime = Date.now() - this.startTime;
-            this.playTime = Date.now() - this.startPlayTime;
-            requestIdleCallback(() => this._update());
-        }
-    }
-    get(t = false) {
-        return t ? (this.playTime / 1000)|0 : (this.elapsedTime / 1000)|0;
-    }
-    getPretty(t = false) {
-        return prettify(t ? this.playTime : this.elapsedTime);
-    }
+  }
+  get(t = false) {
+    return t ? (this.playTime / 1000) | 0 : (this.elapsedTime / 1000) | 0;
+  }
+  getPretty(t = false) {
+    return prettify(t ? this.playTime : this.elapsedTime);
+  }
 }
 
 function prettify(ms) {
-    const h = Math.floor(ms / 3600000),
-        m = Math.floor((ms % 3600000) / 60000),
-        s = Math.floor((ms % 60000) / 1000),
-        l = ((ms % 1000) + "").slice(0, 1);
-    return `${(h+'').padStart(2,'0')}:${(m+'').padStart(2,'0')}:${(s+'').padStart(2,'0')}.${l}`;
+  const h = Math.floor(ms / 3600000),
+    m = Math.floor((ms % 3600000) / 60000),
+    s = Math.floor((ms % 60000) / 1000),
+    l = ((ms % 1000) + "").slice(0, 1);
+  return `${(h + "").padStart(2, "0")}:${(m + "").padStart(2, "0")}:${(s + "").padStart(2, "0")}.${l}`;
 }
 
 function play() {
-    const audio = new Audio('../assets/timer.wav');
-    audio.play();
+  const audio = new Audio("../assets/timer.wav");
+  audio.play();
 }
 
 function con(str) {
-    const [h,m] = str.split(':').map(e=>+e);
-    return new Date().setHours(h,m,0,0);
+  const [h, m] = str.split(":").map((e) => +e);
+  return new Date().setHours(h, m, 0, 0);
 }
 
-var ampm = t => (h=>(x=>x<9?"0"+x:x)(h%12||12)+":"+t.slice(-2)+" "+(h<12?'AM':'PM'))(+t.slice(0,2));
-var id = id => document.getElementById(id);
-var elem = (id_,x) => id(id_).innerText = x;
+var ampm = (t) =>
+  ((h) =>
+    ((x) => (x < 9 ? "0" + x : x))(h % 12 || 12) +
+    ":" +
+    t.slice(-2) +
+    " " +
+    (h < 12 ? "AM" : "PM"))(+t.slice(0, 2));
+var id = (id) => document.getElementById(id);
+var elem = (id_, x) => (id(id_).innerText = x);
